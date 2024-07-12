6bec690967fefa428cc609a0c2779c3e707d5f6d
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-07-12 14:29:52 +0200

    refactor: comment this file for exit error

--- 03_views/nav.php
+++ 03_views/nav.php
@@ -6,17 +6,15 @@ require_once(""); // Añadir requiere a autenticación y UsuariosBD (si lo harem
 // $dotenv = Dotenv\Dotenv::createImmutable("./");
 // $dotenv->load();
 
-if(){ // Comprobamos si las credenciales de usuario son correctas
-    echo "<a href=''></a>"; // Añadir la imagen logo
-    echo "<a href=''>Tarifas y Reservas</a>";
-    echo "<a href=''>Contáctanos</a>";
-    echo "<a href=''>Carrito</a>";
-    echo "<a href=''>LogOut</a>";
-}else{
-    echo "<a href=''></a>"; // Añadir la imagen logo
-    echo "<a href=''>Tarifas y Reservas</a>";
-    echo "<a href=''>Contáctanos</a>";
-    echo "<a href=''>LogIn</a>";
-}
-
-?>
+// if(){ // Comprobamos si las credenciales de usuario son correctas
+//     echo "<a href=''></a>"; // Añadir la imagen logo
+//     echo "<a href=''>Tarifas y Reservas</a>";
+//     echo "<a href=''>Contáctanos</a>";
+//     echo "<a href=''>Carrito</a>";
+//     echo "<a href=''>LogOut</a>";
+// }else{
+//     echo "<a href=''></a>"; // Añadir la imagen logo
+//     echo "<a href=''>Tarifas y Reservas</a>";
+//     echo "<a href=''>Contáctanos</a>";
+//     echo "<a href=''>LogIn</a>";
+// }
